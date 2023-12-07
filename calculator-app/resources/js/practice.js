8892e7c477aa7e1fa0ba90d8778d879c03ddd970
markobojkic <marko.bojkic@uns.ac.rs>
2023-12-07 19:41:09 +0100

    2. cas JS (20. cas) - String concatination, Functions intro, explicit type conversions

--- calculator-app/resources/js/practice.js
+++ calculator-app/resources/js/practice.js
@@ -113,7 +113,7 @@ console.log(`Moj dobijeni rezultat je = ${num2}`); // * preporuka
    4. Druga varijabla neka opise matematicku operaciju koju ste za prvu varijablu koristili (u STRING formatu ispisete samu operaciju).
 */
 
-let someNumber = 4;
+let someNumber = 104;
 
 let description;
 
@@ -121,6 +121,190 @@ someNumber = (someNumber / 2) + (10 - 4) * 3;
 
 console.log(`Rezultat je ${someNumber}`);
 
-description = `(4 / 2) + (10 - 4) * 3`;
+description = `(4 / 2) + (10 - 4) * 3`; // ! ovde smo hard-kodovali broj 4 (tj zakucali vrednost u string)
 
 console.log(description);
+
+
+
+// 2. cas JS-a (20. cas) - 07.12.2023.
+
+console.log('----------- 2. cas JS-a -----------');
+
+let operationDescription = '(' + someNumber + ' / 2) + (10 - 4) * 3'; // * a ovde smo dinamicki prosledili vrednost someNumber varijable (nebitno koja je to vrednost)
+
+console.log(operationDescription);
+
+let templLiterals = `(${someNumber} / 2) + (10 - 4) * 3`; // * bolji nacin ispisivanja vrednosti varijabli (uz pomocu template literala)
+
+console.log(templLiterals);
+
+const a = 5;
+const b = 10;
+console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`);
+
+let goToNewLine = `My string going \nto new line.`;
+
+console.log(goToNewLine);
+
+
+// TODO Zadatak za vezbu:
+/**
+ * 1. Kreirati dve varijable. Jedna neka bude tipa number i neka se zove 
+ *    userInputEx (na koju god vrednost je postavite) i druga neka se naziva 
+ *    result i nemojte je postaviti ni na kakvu vrednost.
+ * 2. Postavite result varijablu na vrednost userInputEx plus neki broj (npr 20).
+ * 3. U naredne 3 linije koda, varijablu result promenite u svakoj 
+ *    narednoj liniji koda operacijama oduzimanja, mnozenja i deljenja 
+ *    nad tom result varijablom i svaki put ispisati vrednost result varijable 
+ *    na konzoli u formatu koji zelite s tim da pre toga stoji String 
+ *    `Current result is: `
+ * 4. Na kraju varijablu result ispisati u okviru alert prozora, 
+ *    a userInputEx varijablu ispisati samo na konzoli.
+*/
+
+const userInputEx = 7;
+let result;
+
+result = userInputEx - 3; // 4
+console.log(`Trenutni rezultat je = ${result}`);
+
+result = result * 4; // stara vrednost result-a je 4, a posto je 4 * 4 = 16, onda je nova result vrednost 16
+// result *= 4; // ! kracin nacin linije koda od gore
+console.log(`Trenutni rezultat je = ${result}`);
+
+result = result / 2; // stara vrednost result-a je 16, a posto je 16/2 = 8, onda je nova result vrednost 8
+// result /= 2; // ! kraci nacin linije koda od gore
+console.log(`Trenutni rezultat je = ${result}`);
+
+// alert(result);
+// console.log(userInputEx);
+
+
+
+// ! kraci nacin za blok koda iz prethodnog zadatka
+
+let aa = 5;
+let bb = 3;
+
+// aa = aa + 10; 
+// console.log(aa); // 15
+
+aa += 10; // ! kraci nacin linije koda od gore
+console.log(aa); // 15
+
+// bb = bb - 2;
+bb -= 2; // ! kraci nacin linije koda od gore
+
+
+
+
+// ! Funkcije
+
+// * Funkcije predstavljaju odredjeni re-usable kod (kod koji mozete iznova koristiti) i prosledjivati joj parametre po zelji
+// broj parametara moze biti od 0 do n 
+function sayHi(name) { // name je parametar ove funkcije
+    // alert('Welcome ' + name);
+    alert(`Welcome ${name}`);
+}
+
+// sayHi('Marko Bojkic');  // argument je 'Marko Bojkic'
+
+// sayHi('Nikola Nikolic');
+
+// sayHi('Pera Peric');
+
+
+// TODO Zadatak za vezbu:
+
+/**
+ * Deklarisati funkciju koja treba da sabere dva broja i kao svoj rezultat
+ * vrati vrednost rezultata sabiranja u formatu: `Result is _RESULT_`
+ * ! HINT: return je kljucna rec koju koristimo kad hocemo da funkcija vraca neku vrednost
+ */
+
+function adding(x, j) {
+    const z = x + j; // 9
+    return `Rezultat sabiranja je = ${z}`; // vracamo string vrednost
+}
+
+let firstResult = adding(3, 6); 
+
+console.log(firstResult);
+// console.log(adding(3,6));
+
+firstResult = adding(100, 200);
+console.log(firstResult);
+
+
+// TODO Zadatak za vezbu
+
+/**
+ * 1. Napraviti funkciju square() koja ce broj prosledjen kao parametar
+ *    podici na kvadrat
+ * 2. Rezultat vratiti u formatu `Squared _number_ is _square-number_`
+ * 3. Pozvati funkciju sa nekom vrednoscu (npr 5)
+ * ! HINT: Ovde mozemo koristiti eksponencijalni operator (**)  -> npr: 2**2
+ */
+
+// ! 1. nacin - ispravan
+// function square(c) {
+//     const res = c**2;
+//     return `Square number is = ${res}`;
+// }
+
+// ! 2. nacin - ispravan
+function square(c) {
+    return `Square number is = ${c**2}`;
+}
+
+console.log(square(2));
+
+console.log(square(4));
+
+console.log('Varijabla aa ima vrednost = ' + aa); // ! izvrsena je konkatinacija, gde smo sa stringom spojili broj i na taj nacin automatski i taj broj pretvorili u string 
+
+
+
+
+// ! Konverzija tipova podataka (pretvaranje jednog tipa podatka u drugi (npr number u string, ili obrnuto))
+
+// ! kada sabiramo broj i string (npr 33 (number) i '2' (string)) , onda se ne dogadja konkretno sabiranje zeljenih brojeva, vec konkatinacija stringova, pa je rezultat '332'
+let t = 33 + '2'; 
+
+console.log(t); // '332' (string)
+
+
+// * eksplicitna konverzija (konverzija koju mi zeljeno navodimo)
+
+// t = 50 + '2'; // zelim 52 a ne '502'
+
+t = 50 + parseInt('2'); // parsiranje stringa u celobrojnu vrednost
+
+console.log(t);
+
+t = 100 + parseFloat('0.32'); // parsiranje stringa u decimalnu vrednost
+
+console.log(t);
+
+t = 5 + parseInt('neki random string');
+
+console.log(t); // NaN  --> skraceno od Not a Number
+
+
+// kraci nacin eksplicitne konverzije stringa u number
+
+t = 50 + +'2'; // ! kraci nacin eksplicitne konverzije stringa u number jeste dodavalje plusa pred string (koji cuva neki broj)
+
+console.log(t);
+
+t = 100 + +'0.32';
+
+console.log(t);
+
+
+// ! obrnuti pravac - number u string
+
+t = t.toString();
+
+console.log(typeof t);
